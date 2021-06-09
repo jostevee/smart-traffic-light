7498e5a479809956f1b473f3a0fb6c5b0fd5e538
Steve Jo <steveeejooo@gmail.com>
2021-06-09 22:39:19 +0700

    Update Commit

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 from time import sleep
 
-width_min=80 # Minimum rectangle width
-height_min=80 # Minimum rectangle height
+width_min = 80 # Minimum rectangle width
+height_min = 80 # Minimum rectangle height
 
-offset=6 #Margin of error between pixels
+offset = 6 # Margin of error between pixels
 
-line_position=450 # Line position (vertically)
+line_position = 450 # Line position (vertically)
 
-delay= 60 # video FPS
+delay = 60 # Video FPS
 
 detect = []
-cars= 0
+cars = 0
 
 	
 def center_point(x, y, w, h):
@@ -32,34 +32,34 @@ while True:
     grey = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(grey,(3,3),5)
     img_sub = subtract.apply(blur)
-    dilat = cv2.dilate(img_sub,np.ones((5,5)))
+    dilation = cv2.dilate(img_sub,np.ones((5,5)))
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    dilatada = cv2.morphologyEx (dilat, cv2. MORPH_CLOSE , kernel)
-    dilatada = cv2.morphologyEx (dilatada, cv2. MORPH_CLOSE , kernel)
-    contorno,h=cv2.findContours(dilatada,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    dilatation = cv2.morphologyEx (dilation, cv2. MORPH_CLOSE , kernel)
+    dilatation = cv2.morphologyEx (dilatation, cv2. MORPH_CLOSE , kernel)
+    contour,h = cv2.findContours(dilatation,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
     cv2.line(frame1, (25, line_position), (1200, line_position), (255,127,0), 3) 
-    for(i,c) in enumerate(contorno):
+    for(i,c) in enumerate(contour):
         (x,y,w,h) = cv2.boundingRect(c)
-        validar_contorno = (w >= width_min) and (h >= height_min)
-        if not validar_contorno:
+        validate_outline = (w >= width_min) and (h >= height_min)
+        if not validate_outline:
             continue
 
         cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)        
-        centro = center_point(x, y, w, h)
-        detect.append(centro)
-        cv2.circle(frame1, centro, 4, (0, 0,255), -1)
+        center = center_point(x, y, w, h)
+        detect.append(center)
+        cv2.circle(frame1, center, 4, (0, 0,255), -1)
 
         for (x,y) in detect:
             if y<(line_position+offset) and y>(line_position-offset):
                 cars+=1
                 cv2.line(frame1, (25, line_position), (1200, line_position), (0,127,255), 3)  
                 detect.remove((x,y))
-                print("car is detectted : "+str(cars))        
+                print("car is detected : "+str(cars))        
        
     cv2.putText(frame1, "VEHICLE COUNT : "+str(cars), (450, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),5)
     cv2.imshow("Video Original" , frame1)
-    cv2.imshow("detecttar",dilatada)
+    cv2.imshow("dilatation",dilatation)
 
     if cv2.waitKey(1) == 27:
         break
